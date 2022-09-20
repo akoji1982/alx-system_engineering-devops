ba062d0b3d0c38061954472ed20ccdaff39921f3
akoji@1982 <akoji1982@gmail.com>
2022-09-20 02:13:37 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Installs a package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+# create a file
+node default {
+  file { '/tmp/school':
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+  }
 }
+
+
